a722f9db9a097eaa56030c0628c801663899becf
Ian Liao <ianliao@eztravel.com.tw>
2016-08-22 17:52:44 +0800

    Add "VD 微觀情形" tab

--- js/main.js
+++ js/main.js
@@ -31,15 +31,17 @@
   }
 
   var state = {
-    category: "normal",
+    category: 'normal',
     month: 1,
     week: [],
+    week2: '1',
     interval: [9, 17],
-    oilprice: "1",
-    rainfall: "1",
-    temperature: "1",
+    oilprice: '1',
+    rainfall: '1',
+    temperature: '1',
     date: $.date(new Date())
   }
+  window.state = state
 
   var $month = $('#month')
   var monthContent = '<div class="btn-group" role="group">' +
@@ -48,10 +50,13 @@
     '</label>' +
     '</div>'
 
+  var weekAry = ['一', '二', '三', '四', '五', '六', '日']
+  var weekHtmlAry = []
   var $week = $('#week')
+  var $week2 = $('#week2')
   var weekContent = '<div class="btn-group" role="group">' +
     '<label class="btn btn-primary">' +
-    '<input type="checkbox" name="week" autocomplete="off" value="#{val}">#{label}' +
+    '<input type="#{type}" name="#{name}" autocomplete="off" value="#{val}">#{label}' +
     '</label>' +
     '</div>'
 
@@ -68,6 +73,18 @@
   jQuery(function ($) {
 
     $('.nav-tabs a:first').tab('show')
+    //$('.nav-tabs a:last').tab('show')
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      if (!VDMap) {
+        VDMap = new google.maps.Map(document.getElementById('vd-map'), {
+          center: {
+            lat: 25.041650,
+            lng: 121.543801
+          },
+          zoom: 12
+        })
+      }
+    })
 
     $('#category').find('.btn').on('click', function () {
       state['category'] = $(this).find('input[name="category"]').val()
@@ -87,26 +104,32 @@
 
     // 月份.
     var monthHtmlAry = []
-
     for (var i = 1; i < 13; i++) {
       monthHtmlAry.push(monthContent.replace(/#\{label\}/g, '' + i).replace(/#\{val\}/g, '' + i))
     }
     $month.find('.btn-group').html(monthHtmlAry.join('')).children(':first').find('.btn').click()
 
     // 星期.
-    var weekHtmlAry = []
-    $.map(['一', '二', '三', '四', '五', '六', '日'], function (v, i) {
-      weekHtmlAry.push(weekContent.replace(/#\{label\}/g, v).replace(/#\{val\}/g, i + 1))
+    $.map(weekAry, function (v, i) {
+      weekHtmlAry.push(
+        weekContent.replace(/#\{type\}/g, 'checkbox')
+                   .replace(/#\{name\}/g, 'week')
+                   .replace(/#\{label\}/g, v)
+                   .replace(/#\{val\}/g, i + 1)
+      )
     })
 
     // 預設禮拜一
     $week.find('.btn-group').html(weekHtmlAry.join(''))
-      .find('.btn').on('click', function () {
-        setTimeout(function () {
-          syncWeekState()
-        }, 0)
-      }).end()
-      .children(':first').find('.btn').click()
+          .find('.btn').on('click', function () {
+            setTimeout(function () {
+              state.week.length = 0
+              $week.find('.btn.active').find('input').each(function (index) {
+                state.week.push($(this).val())
+              })
+            }, 0)
+          }).end()
+          .children(':first').find('.btn').click()
 
     // 時段.
     $interval.slider({
@@ -183,14 +206,45 @@
         state.date = e.format()
       })
 
+    
+    // --------- VD 微觀情形 ---------
+    $('.selectpicker').selectpicker({
+      style: 'btn-primary',
+      width: '85%',
+      size: 4
+    })
+    
+    $('#type').find('.btn').on('click', function () {
+      state['type'] = $(this).find('input[name="type"]').val()
+      switch (state['type']) {
+      case 'year':
+        $week2.removeClass('hide-block')
+        break
+      case 'rainfall':
+        $week2.addClass('hide-block')
+        break
+      }
+    })
+    
+    // 星期.
+    weekHtmlAry.length = 0
+    $.map(weekAry, function (v, i) {
+      weekHtmlAry.push(
+        weekContent.replace(/#\{type\}/g, 'radio')
+                   .replace(/#\{name\}/g, 'week2')
+                   .replace(/#\{label\}/g, v)
+                   .replace(/#\{val\}/g, i + 1)
+      )
+    })
+    $week2.find('.btn-group').html(weekHtmlAry.join(''))
+          .find('.btn').on('click', function () {
+            state.week2 = $(this).find('input[name="week2"]').val()
+          }).end()
+          .children(':first').find('.btn').click()
+    
+
   })
 
-  function syncWeekState() {
-    state.week.length = 0
-    $week.find('.btn.active').find('input').each(function (index) {
-      state.week.push($(this).val())
-    })
-  }
 
   function syncState() {
     $('.sync').each(function (index) {
